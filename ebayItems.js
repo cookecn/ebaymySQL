ffd7f6fd5e3ba9e26cd29ffad23c3fe418d0511d
Cameron Forrest <44378657+CameronForrest@users.noreply.github.com>
2019-04-04 21:09:40 -0500

    Create ebayItems.js

---
+++ ebayItems.js
@@ -0,0 +1,43 @@
+function createProduct() {
+  console.log("Inserting a new product...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      name: "macbook",
+      price: 300.00,
+      
+    },
+
+    {
+      name: "iphone",
+      price: 100.00,
+      
+    },
+
+    {
+      name: "corvette",
+      price: 2000.00,
+      
+    },
+
+    {
+      name: "shirts",
+      price: 3.00,
+      
+    },
+
+    {
+      name: "gokus power level",
+      price: 9001.00,
+      
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
